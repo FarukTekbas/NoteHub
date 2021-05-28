ca84fe2c4e37c7e3dbb85dfa1cbc7d619d6c3654
FarukTekbas <77430753+FarukTekbas@users.noreply.github.com>
2021-05-28 14:08:13 +0300

    Add files via upload

---
+++ NoteHub.API/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteHub.API
+{
+    public static class Constants
+    {
+        public const string DEFAULT_EMAIL = "admin@example.com";
+        public const string DEFAULT_PASSWORD = "P@ssword1";
+        public const string AUTH_SIGNING_KEY = "yA24*FASD₺d2*kaf___342L$``d*f*k3a??-2134af,*/--asfd";
+
+        public static class Roles
+        {
+            public const string ADMIN = "Admin";
+        }
+    }
+}
